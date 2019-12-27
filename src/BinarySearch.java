77ed8a8809746502b196e48a26678af78e2ffa5d
Piotr <piotreksl2002@go2.pl>
2019-12-27 21:56:41 +0100

    Binary Search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,4 +1,28 @@
+
+
 public class BinarySearch {
 
-    
+    public static int rank(int key, int[] a){
+        int lo = 0;
+        int hi = a.length -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi -lo) /2;
+
+
+            if (key < a[mid]) hi= mid - 1;
+            else if (key > a[mid]) lo = mid +1;
+            else return  mid;
+        }
+        return -1;
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] test = {1,4,8,12,15,17};
+        System.out.println(rank(1, test));
+    }
 }
